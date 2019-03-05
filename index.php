5d8580af2462500b67f3b18ac6d1d7641e84b812
kraisorn1597 <kraisorn1597@gmail.com>
2019-03-05 10:29:20 +0700

    bot05

--- index.php
+++ index.php
@@ -1,36 +1,72 @@
 <?php
-require_once('./vendor/autoload.php');
-// Namespace
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot;
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-$channel_token =
-    'Xh0zP+2rNt9B/xlU/h5i3w7JW6PuNGYoSj0IpdCl4OrIcWUJijAiNs2REmK+IeRYHq48fASa/gLXqg5qaJtCKgBDkxiVTJXSTg++oIfHFRdsfJ/KClZFFhTBaBgrvZNA7jQdVdZMPVk141SbkfkXCQdB04t89/1O/w1cDnyilFU=
-';
-$channel_secret = '59acba73be9e1e9e395b3faef7711430';
-// Get message from Line API
+$accessToken = "Xh0zP+2rNt9B/xlU/h5i3w7JW6PuNGYoSj0IpdCl4OrIcWUJijAiNs2REmK+IeRYHq48fASa/gLXqg5qaJtCKgBDkxiVTJXSTg++oIfHFRdsfJ/KClZFFhTBaBgrvZNA7jQdVdZMPVk141SbkfkXCQdB04t89/1O/w1cDnyilFU=
+";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+
 $content = file_get_contents('php://input');
-$events = json_decode($content, true);
-if (!is_null($events['events'])) {
-// Loop through each event
-    foreach ($events['events'] as $event) {
-// Line API send a lot of event type, we interested in message only.
-        if ($event['type'] == 'message') {
-            switch($event['message']['type']) {
-                case 'text':
-                    // Get replyToken
-                    $replyToken = $event['replyToken'];
-                    // Reply message
-                    $respMessage = 'Hello, your message is '. $event['message']['text'];
-                    $httpClient = new CurlHTTPClient($channel_token);
-                    $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-                    $textMessageBuilder = new TextMessageBuilder($respMessage);
-                    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
-                    break;
-            }
-        }
-    }
+$arrayJson = json_decode($content, true);
+
+$arrayHeader = array();
+$arrayHeader[] = "Content-Type: application/json";
+$arrayHeader[] = "Authorization: Bearer {$accessToken}";
+
+//รับข้อความจากผู้ใช้
+$message = $arrayJson['events'][0]['message']['text'];
+#ตัวอย่าง Message Type "Text"
+if($message == "สวัสดี"){
+    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+    $arrayPostData['messages'][0]['type'] = "text";
+    $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+    replyMsg($arrayHeader,$arrayPostData);
 }
-
-echo 'Hello';
-
+#ตัวอย่าง Message Type "Sticker"
+else if($message == "ฝันดี"){
+    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+    $arrayPostData['messages'][0]['type'] = "sticker";
+    $arrayPostData['messages'][0]['packageId'] = "2";
+    $arrayPostData['messages'][0]['stickerId'] = "46";
+    replyMsg($arrayHeader,$arrayPostData);
+}
+#ตัวอย่าง Message Type "Image"
+else if($message == "รูปน้องแมว"){
+    $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
+    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+    $arrayPostData['messages'][0]['type'] = "image";
+    $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
+    $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
+    replyMsg($arrayHeader,$arrayPostData);
+}
+#ตัวอย่าง Message Type "Location"
+else if($message == "พิกัดสยามพารากอน"){
+    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+    $arrayPostData['messages'][0]['type'] = "location";
+    $arrayPostData['messages'][0]['title'] = "สยามพารากอน";
+    $arrayPostData['messages'][0]['address'] =   "13.7465354,100.532752";
+    $arrayPostData['messages'][0]['latitude'] = "13.7465354";
+    $arrayPostData['messages'][0]['longitude'] = "100.532752";
+    replyMsg($arrayHeader,$arrayPostData);
+}
+#ตัวอย่าง Message Type "Text + Sticker ใน 1 ครั้ง"
+else if($message == "ลาก่อน"){
+    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+    $arrayPostData['messages'][0]['type'] = "text";
+    $arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
+    $arrayPostData['messages'][1]['type'] = "sticker";
+    $arrayPostData['messages'][1]['packageId'] = "1";
+    $arrayPostData['messages'][1]['stickerId'] = "131";
+    replyMsg($arrayHeader,$arrayPostData);
+}
+function replyMsg($arrayHeader,$arrayPostData){
+    $strUrl = "https://api.line.me/v2/bot/message/reply";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$strUrl);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+    curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec($ch);
+    curl_close ($ch);
+}
+exit;
+?>
